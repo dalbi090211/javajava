dea5c269939a6328d0c760341e2ed71674254121
dalbi090211 <trump0911@naver.com>
2022-09-17 14:54:07 +0000

    [asdf]

---
+++ Selection.java
@@ -0,0 +1,40 @@
+public class Selection {
+    public static void main(String args[]){
+        /*
+         * 1.국어와 영어 점수를 입력 받아 두 점수의 합이 120점 이상이면 합격을, 
+         * 그렇지 않으면 불합격을 출력하는 프로그램을 작성하시오.
+         * 입력을 어떻게 받을지 몰라서 몰라요
+         */
+        int Score_math = 0;
+        int Score_english = 0;
+        //대충 입력받는 부분
+        if(Score_math + Score_english > 120){
+            System.out.println("합격");
+        }
+        else{
+            System.out.println("불합격");
+        }
+        
+
+        /*
+         * 3. 유권자 수와 투표자 수를 입력 받아 투표율이 50% 이상인 경우 
+         * “유효 투표입니다”를 출력하는 프로그램을 작성하시오.
+         */
+        int All_num = 0;
+        int Engage_num = 0;
+        //대충 입력받는 부분
+        if(Engage_num > All_num * 0.5){
+            System.out.println("유효 투표입니다");
+        }
+
+        
+        /*
+         * 5. 한 사람당 8000원인 영화관에서 10명 이상일 경우 10%, 20명 이상일 경우 20%를 할인해 주고 있다. 
+         * 사람 수를 입력 받아 지불해야 하는 총 비용을 출력하는 프로그램을 작성하시오.
+        { 실행 결과 }
+        사람 수 : 00명
+        총 비용(00%할인 적용) : 0000000원
+         */
+
+    }
+}
