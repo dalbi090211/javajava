a083c785c28ccf0d8cdd5512ec527a63b2849516
dalbi090211 <trump0911@naver.com>
2022-10-03 14:38:39 +0000

    [asdf]

--- Roop.java
+++ Roop.java
@@ -75,6 +75,14 @@ import java.util.Scanner;
 5월은 봄에 해당됩니다 }
 */
 
+class SyntaxException extends Exception {   //문법오류 시 예외를 처리하기 위한 사용자 정의 예외 클래스
+    public SyntaxException(){   //오류메세지를 넣지 않는 경우
+    }
+    public SyntaxException(String message){ //오류메세지를 넣는 경우
+        super(message);
+    }
+}
+
 public class Roop {
     //전역함수 선언
     public static Scanner sc  = new Scanner(System.in);
@@ -92,14 +100,32 @@ public class Roop {
         }
         System.out.println("어디까지 구할까요?");
         max = sc.nextInt();
-        for(i = 0; i < max / factor; i++){
+        if(max < 1){
+            throw new SyntaxException("1보다 큰 숫자를 입력해주세요.");
+        }
+        else if(max < factor){
+            throw new SyntaxException("배수를 구할 숫자보다 큰 값을 입력해주세요.");
+        }
+        for(i = 1; i < max / factor + 1; i++){
             temp += i * factor;
         }
         System.out.println(factor + "의 배수의 합은 : " + temp + "입니다.");
     }
-    public static void two(int end, int repeat_num){
+    public static void two() throws SyntaxException{
+        int repeat_num = 0;
+        int end = 0;
+        System.out.println("몇 단을 출력할까요?(최대 15) ");
+        repeat_num = sc.nextInt();
+        if(repeat_num > 15 || repeat_num < 1){
+            throw new SyntaxException("조건에 맞는 정수를 입력해주세요.");
+        }
+        System.out.println("어디까지 출력할까요?(최대 20) ");
+        end = sc.nextInt();
+        if(end > 20 || end < 1){
+            throw new SyntaxException("조건에 맞는 정수를 입력해주세요.");
+        }
         System.out.println("{** " + repeat_num + "단 **");
-        for(i = 0; i < end + 1; i++){
+        for(i = 1; i < end + 1; i++){
             if( i == end){
                 System.out.println(repeat_num + "*" + i + "=" + (repeat_num * i) + "}");
             }
@@ -130,10 +156,10 @@ public class Roop {
                 case 1 : 
                 one();
                 break;
-                /* 
                 case 2 : 
                 two();
                 break;
+                /* 
                 case 3 : 
                 three();
                 break;
@@ -154,30 +180,24 @@ public class Roop {
             try{
                 System.out.println("다시 시작하려면 코드번호를 아니라면 X를 입력해주세요.");
                 code_num = sc.nextInt();
-            }
-            catch(InputMismatchException e){    //X혹은 숫자가 아닌 다른 타입을 넣은 경우
-                throw new SyntaxException();
-            }
-            finally{
-                if(code_num >= 1 || code_num <= 7){ //제대로 된 정수가 들어간 경우
-                    System.out.print("\033[H\033[2J");
+                if(code_num >= 1 && code_num <= 7){ //제대로 된 정수가 들어간 경우
                     start_code(code_num);
                 }
                 else{   //정수인데 제대로 된 값이 아닌 경우
                     throw new SyntaxException("1~7사이의 숫자를 입력해주세요.");
                 }
             }
+            catch(InputMismatchException e){    //X혹은 숫자가 아닌 다른 타입을 넣은 경우
+                throw new SyntaxException();
+            }
             
-        
     }
-
-
     public static void main(String args[]){
         int code_num = 0;
         try{
             System.out.println("실행할 코드의 번호를 정수로 입력해주세요.");
             code_num = sc.nextInt();
-            if(code_num >= 1 || code_num <= 7){
+            if(code_num >= 1 && code_num <= 7){
                 start_code(code_num);
             }
             else{
@@ -185,9 +205,8 @@ public class Roop {
             }
         }
         catch(SyntaxException e){
-            if(e.getMessage().length() != 0){   //오류메세지가 없을 경우 출력하지 않음
-                System.out.println(e.getMessage());  
-                System.out.println(e.getMessage().length()); 
+            if(e.getMessage() != null){   //오류메세지가 없을 경우 출력하지 않음
+                System.out.println(e.getMessage()); 
             }
             System.out.println("프로그램이 종료되었습니다.");
         }
