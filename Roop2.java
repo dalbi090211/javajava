af76374121b139c5043082c35659a6a7c49dbdb6
dalbi090211 <trump0911@naver.com>
2022-10-13 14:11:33 +0000

    [sdf]

---
+++ Roop2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Roop2 {
+    public static java.util.Scanner sc  = new java.util.Scanner(System.in);
+    public static int i = 0;
+    public static int j = 0;
+    public static int trial = 0;
+
+    public class first {
+        int temp = 0;
+        void get_max(int max) {
+            
+        }
+    
+        void print_num{
+            for(i = 0; i < max + 1; i++){
+                temp += i;
+                if(i % 10 == 0){
+                    System.out.println("1 - " + i + "까지의 합 : " + temp);
+                }
+            }
+        }
+        long subtract(long a, long b) { return a - b; }
+        long multiply(long a, long b) { return a * b; }
+        double divide(double a, double b) { return a / b; } 
+    }
+
+}
+
